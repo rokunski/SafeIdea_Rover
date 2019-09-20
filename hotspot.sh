1c9cd20a0eee115c592542262aeb9bb1749dff09
rokunski <r.okunski@wp.pl>
2019-09-20 17:28:09 +0200

    update hotspot file

--- hotspot.sh
+++ hotspot.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-if test "$~/Documents/create_ap"
-then
-l="$(ls /sys/class/net/ | grep wl)"
-sudo create_ap $l $l Roverr silazik123
-else
+
+echo -e "\e[32mWe need to install some packages"
+echo -e "\e[0m"
 sudo apt install hostapd iproute2 iw haveged dnsmasq iptables procps bash util-linux
-cd ~/Documents
-git clone https://github.com/oblique/create_ap
-cd create_ap/
-sudo make install
-l="$(ls /sys/class/net/ | grep wl)"
-sudo create_ap $l $l Roverr silazik123
+
+echo""
+
+if ! [ -x "$(command -v create_ap)" ]; then
+  cd ~/Documents
+  git clone https://github.com/oblique/create_ap
+  cd create_ap
+  sudo make install > /dev/null
 fi
 
+echo ""
+echo -e "\e[32mStarting hotspot"
+echo -e "\e[0m"
+l="$(ls /sys/class/net/ | grep wl)"
+sudo create_ap $l $l Roverr silazik123
