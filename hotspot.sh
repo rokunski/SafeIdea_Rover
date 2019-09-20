e9ff3ca6d28b26e6851f0d86f74e9e44f15b9a68
Maffek <maf0208@interia.pl>
2019-09-20 14:49:40 +0200

    Update hotspot.sh
    Check if it was installed

--- hotspot.sh
+++ hotspot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+if test "$~/Documents/create_ap"
+then
+l="$(ls /sys/class/net/ | grep wl)"
+sudo create_ap $l $l Roverr silazik123
+else
 sudo apt install hostapd iproute2 iw haveged dnsmasq iptables procps bash util-linux
 cd ~/Documents
 git clone https://github.com/oblique/create_ap
 cd create_ap/
 sudo make install
-ifconfig
 l="$(ls /sys/class/net/ | grep wl)"
 sudo create_ap $l $l Roverr silazik123
+fi
+
