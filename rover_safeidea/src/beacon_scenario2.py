18e9b6eedcd43276c2ad5c8431b2344af950eceb
rokunski <r.okunski@wp.pl>
2019-11-17 22:53:06 +0100

    beacon finder - no obstacle

---
+++ rover_safeidea/src/beacon_scenario2.py
@@ -0,0 +1,309 @@
+#! /usr/bin/env python3
+
+import rospy
+import time
+import numpy as np
+from sensor_msgs.msg import LaserScan, Imu
+from geometry_msgs.msg import Vector3, Twist
+from nav_msgs.msg import Odometry
+from scipy.spatial.transform import Rotation as R
+import random
+import threading
+from enumerate import State
+
+
+BeaconPoints = [Vector3(-20,20,0), Vector3(20,7,0), Vector3(15,-22,0), Vector3(-14,-16,0)]
+BeaconPoints = [Vector3(-5,5,0), Vector3(5,5,0), Vector3(5,-5,0), Vector3(-5,-5,0)]
+
+class beacon:
+
+    def __init__(self):
+
+        rospy.init_node("beacon", disable_signals=True)
+        rospy.loginfo("Setting up BEACON Scenario node .....")
+
+        self.robot_pose = Vector3(0.,0.,0.)
+        self.robot_rot = Vector3(0.,0.,0.)
+        self.turn_l = 0
+        self.turn_p = 0
+        self.forward_l = 0
+        self.forward_p = 0
+
+        self.aim = BeaconPoints[np.random.randint(0,4)]
+        print(self.aim.x, " ", self.aim.y)
+
+        self.stan = State.DIRECTION_SEARCH
+
+        self.attenuation = 3
+        self.f = 2400000000
+        self.c = 300000000
+        self.one_meter = 10 * self.attenuation * np.log10(4 * np.pi * self.f / self.c )
+        print(self.one_meter)
+
+        self.sig = 0
+        self.first = True
+        self.time = 0
+        self.l = []
+        self.ang = 0
+        self.best = 1000
+
+        self.sub_lidar = rospy.Subscriber('/rover/laser/scan', LaserScan, self.lidar_receive)
+        self.sub_ground_truth = rospy.Subscriber('/ground_truth/state', Odometry, self.ground_receive)
+        self.sub_imu = rospy.Subscriber("/rover/imu", Imu, self.imu_receive)
+        self.pub_cmd = rospy.Publisher("/rover/cmd_vel", Twist, queue_size =10)
+
+        rospy.sleep(1)
+
+    def lidar_receive(self, message):
+        data = message.ranges
+        angle_min = message.angle_min
+        angle_max = message.angle_max
+        angle_increment = message.angle_increment
+
+        min_left = 1000
+        min_leftc = 1000
+        min_center = 1000
+        min_rightc = 1000
+        min_right = 1000
+        for i in range(0, int(len(data) / 4)):
+            if min_right > data[i]:
+                min_right = data[i]
+
+        for i in range(int(len(data) / 4), int(len(data) / 2) - 20):
+            if min_rightc > data[i]:
+                min_rightc = data[i]
+
+        for i in range(int(len(data) / 2) - 20, int(len(data) / 2) + 20):
+            if min_center > data[i]:
+                min_center = data[i]
+
+        for i in range(int(len(data) / 2) + 20, int(len(data) / 4 * 3)):
+            if min_leftc > data[i]:
+                min_leftc = data[i]
+
+        for i in range(int(len(data) / 4 * 3), len(data)):
+            if min_left > data[i]:
+                min_left = data[i]
+
+        mini = min([min_left, min_leftc, min_center, min_rightc, min_right])
+        idx = np.argmin(np.array([min_left, min_leftc, min_center, min_rightc, min_right]))
+
+        do = False
+
+        if mini < 1 and 0 < idx < 4:
+            do = True
+            # self.turn_p = 0
+            if idx == 1:
+                self.forward_l = -0.3
+                self.turn_l = -0.15
+                #self.time = time.time()
+                #self.steer_time = 3
+            elif idx == 3:
+                self.forward_l = -0.3
+                self.turn_l = 0.15
+                #self.time = time.time()
+                #self.steer_time = 3
+            else:
+                self.forward_l = -0.3
+                self.turn_l = -0.15
+                #self.time = time.time()
+                #self.steer_time = 6
+        if 2.5 > mini > 1 and 0 < idx < 4:
+            do = True
+            if idx == 1:
+                self.forward_l = -0.3
+                self.turn_l = -0.08
+                #self.time = time.time()
+                #self.steer_time = 2
+            elif idx == 3:
+                self.forward_l = -0.3
+                self.turn_l = 0.08
+                #self.time = time.time()
+                #self.steer_time = 2
+            else:
+                self.forward_l = -0.3
+                self.turn_l = -0.08
+                #self.time = time.time()
+                #self.steer_time = 4
+        if mini < 1 and (idx == 0 or idx == 4):
+            do = True
+            if idx == 0:
+                self.turn_l = -0.08
+            else:
+                self.turn_l = 0.08
+        if not do:
+            self.turn_l = 0
+            self.forward_l = 0
+
+
+    def imu_receive(self,message):
+        orientation_q = message.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        r = R.from_quat(orientation_list)
+        euler = r.as_rotvec()
+
+        self.robot_rot.x = euler[0]
+        self.robot_rot.y = euler[1]
+        self.robot_rot.z = euler[2]
+
+    def ground_receive(self,message):
+        self.robot_pose.x = message.pose.pose.position.x
+        self.robot_pose.y = message.pose.pose.position.y
+        self.robot_pose.z = message.pose.pose.position.z
+        if self.sig == 0:
+            self.measure = True
+
+        l = self.length(self.robot_pose.x, self.robot_pose.y, self.aim.x, self.aim.y)
+
+        self.sig = -(self.one_meter + self.attenuation * 10 * np.log10(l))
+        rrr = random.gauss(0,0.001)
+        self.sig += self.sig * rrr
+
+
+    @staticmethod
+    def length(x1,y1,x2,y2):
+        return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+    @staticmethod
+    def angle(x1,y1,x2,y2 ):
+        return np.arctan2(y2-y1, x2-x1)
+
+    def steering(self):
+        msg = Twist()
+        msg.linear.x = self.turn_p
+        msg.angular.z = self.forward_p
+        self.pub_cmd.publish(msg)
+
+    def search(self):
+        while True:
+            #rospy.loginfo(self.stan)
+            try:
+                if self.stan == State.DIRECTION_SEARCH:
+                    if self.first:
+                        rospy.loginfo("Starting finding direction ...")
+                        self.l = []
+                        self.time = time.time()
+                        self.first = False
+                        self.best = 1000
+                    if time.time() - self.time < 8:
+                        self.forward_p = -0.3
+                        self.turn_p = 0
+                        self.l.append(10 ** ((0 - self.sig - self.one_meter) / 10 / self.attenuation))
+                        if self.l[-1] < 0.9:
+                            self.turn_p = 0
+                            self.forward_p = 0
+                            self.first = True
+                            self.stan = State.FINISH
+                            rospy.loginfo("Finishing ...")
+                    else:
+                        self.forward_p = 0
+                        self.turn_p = 0
+                        self.first = True
+                        self.stan = State.CHECKING
+                        rospy.loginfo("I'm done first state ...")
+                elif self.stan == State.CHECKING:
+                    if self.l[0] > self.l[-1]:
+                        self.stan = State.RIDE
+                        rospy.loginfo("Pushing forward ....")
+                    else:
+                        rospy.loginfo("I'm turning around")
+                        self.stan = State.ROT_180
+                elif self.stan == State.RIDE:
+                    if self.first:
+                        rospy.loginfo("Starting riding")
+                        self.l = []
+                        self.best = 1000
+                        self.first = False
+                    self.forward_p = -0.3
+                    self.turn_p = 0
+                    self.l.append(10 ** ((0 - self.sig - self.one_meter) / 10 / self.attenuation))
+                    if self.l[-1] < self.best:
+                        self.best = self.l[-1]
+                    if self.l[-1] < 0.9:
+                        self.turn_p = 0
+                        self.forward_p = 0
+                        self.first = True
+                        rospy.loginfo("Finishing ...")
+                        self.stan = State.FINISH
+                    elif len(self.l) > 20:
+                        if self.l[-1] - self.best > 0.2:
+                            self.turn_p = 0
+                            self.forward_p = 0
+                            self.first = True
+                            self.stan = State.ROT_90
+                            rospy.loginfo("Worse, 90 degree rotation ...")
+                elif self.stan == State.ROT_180:
+                    if self.first:
+                        #self.time = time.time()
+                        rospy.loginfo("Starting rotating 180 ...")
+                        self.first = False
+                        self.ang = self.robot_rot.z
+                        if self.ang < 0:
+                            self.ang += np.pi
+                        else:
+                            self.ang -= np.pi
+                    if np.abs(self.ang - self.robot_rot.z) > 0.01:
+                        self.forward_p = -0.3
+                        self.turn_p = 0.13
+                    else:
+                        self.forward_p = 0
+                        self.turn_p = 0
+                        self.first = True
+                        self.stan = State.RIDE
+                        rospy.loginfo("Finish rotation 180 ....")
+                elif self.stan == State.ROT_90:
+                    if self.first:
+                        #self.time = time.time()
+                        rospy.loginfo("Starting rotating 90 ...")
+                        self.first = False
+                        self.ang = self.robot_rot.z
+                        self.ang += np.pi/2
+                        if self.ang > np.pi:
+                            self.ang -= 2 * np.pi
+                    if np.abs(self.ang - self.robot_rot.z) > 0.01:
+                        self.forward_p = -0.3
+                        self.turn_p = -0.13
+                    else:
+                        self.forward_p = 0
+                        self.turn_p = 0
+                        self.first = True
+                        rospy.loginfo("Finish rotation 90 ...")
+                        self.stan = State.DIRECTION_SEARCH
+                elif self.stan == State.FINISH:
+                    self.forward_p = 0
+                    self.turn_p = 0
+                    rospy.loginfo("I'm here")
+
+            except KeyboardInterrupt:
+                msg = Twist()
+                msg.linear.x = 0
+                msg.angular.z = 0
+                self.pub_cmd.publish(msg)
+                break
+        return
+
+    def run(self):
+        rate = rospy.Rate(30)
+        self.read = threading.Thread(target=self.search).start()
+        while not rospy.is_shutdown():
+            try:
+                self.steering()
+                rate.sleep()
+            except Exception as e:
+                print(e)
+            except KeyboardInterrupt:
+                #self.read.stop()
+                msg = Twist()
+                msg.linear.x = 0
+                msg.angular.z = 0
+                self.pub_cmd.publish(msg)
+                rospy.loginfo("Ending.........")
+                rospy.signal_shutdown('Quit')
+
+
+
+
+if __name__ == "__main__":
+    sc = beacon()
+    sc.run()
+
--- rover_safeidea/src/enumerate.py
+++ rover_safeidea/src/enumerate.py
@@ -8,9 +8,21 @@ class Joy(IntEnum):
     CONNECTED = 2
     NOT_WORKING = 3
 
+
 class Motor(IntEnum):
     NOT_WORKING = 1
     FORWARD = 2
     BACKWARD = 3
     BREAKING = 4
     WAITING = 5
+
+
+class State(IntEnum):
+    DIRECTION_SEARCH = 0
+    ROT_180 = 1
+    ROT_90 = 2
+    AVOID = 3
+    NEXT = 4
+    RIDE = 5
+    CHECKING = 6
+    FINISH = 7
